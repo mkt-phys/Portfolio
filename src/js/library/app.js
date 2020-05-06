e179957c4d3e95db006e1a303097dc96b5366c19
森 真輝人 / Makito Mori <makito.mori@bigtreetc.com>
2020-05-06 15:42:03 +0900

    5月5日

---
+++ src/js/library/app.js
@@ -0,0 +1,48 @@
+// VANTA.DOTS({
+//     el: "#your-element-selector",
+//     mouseControls: true,
+//     touchControls: true,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00,
+//     color: 0x2820ff,
+//     color2: 0x5d20ff,
+//     size: 6.80,
+//     spacing: 38.00
+// })
+
+// VANTA.BIRDS({
+//     el: "#your-element-selector",
+//     mouseControls: true,
+//     touchControls: true,
+//     minHeight: 300.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00,
+//     birdSize: 2.10,
+//     separation: 100.00
+// })
+
+
+VANTA.RINGS({
+    el: "#vanta-erea",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x0,
+    color: 0x1934e0
+})
+
+// VANTA.WAVES({
+//     el: "#your-element-selector",
+//     mouseControls: true,
+//     touchControls: true,
+//     minHeight: 300.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00
+// })
--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
 import "./main.scss";
-
+// import Three from "vanta/vender/three.r95.min"
+// import Rings from "vanta/vendor/three.r95.min"
+import Three from "./js/library/three.r95.min";
+import Rings from "./js/library/vanta.rings.min";
+import Appaaaaa from "./js/library/app";
 // ページ読み込み時に実行したい処理
-window.onload = () => {};
+window.onload = () => {
+    Three();
+    Rings();
+    Appaaaaa();
+};
